d556849802f0b877410dd8ca027d2ca524239349
vld <vladk6813@gmail.com>
2023-11-25 23:30:52 +0300

    actual init

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def art():
-    print("art")
-    print('ur')
-def vlad():
-    print('Vlad')
+import requests
+from bs4 import BeautifulSoup
+import json
+import os
+
+offset = 0
+limit = 500
+
+
+def get_pdf(url, title):
+    html = requests.get(url)
+    pdf = open(f'C:/Users/{os.getlogin()}/Desktop/LLM4/{title}.pdf', 'wb')
+    pdf.write(html.content)
