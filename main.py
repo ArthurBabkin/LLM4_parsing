4232bed566338908f2cbebab396d487246ae643e
Arthur <70059647+ArthurBabkin@users.noreply.github.com>
2023-11-26 13:54:57 +0300

    Added Artur

--- main.py
+++ main.py
@@ -6,21 +6,62 @@ import os
 offset = 0
 limit = 500
 
+articles_arXiv_ID = set()
+articles_paper_ID = set()
+articles_titles = set()
 
-def get_pdf(url, title):
+
+def get_mmd(url, title):
     html = requests.get(url)
-    pdf = open(f'C:/Users/{os.getlogin()}/Desktop/LLM4/{title}.pdf', 'wb')
+    pdf = open(f'C:/Users/{os.getlogin()}/Desktop/LLM4/DataScience/Articles/{title}.pdf', 'wb')
     pdf.write(html.content)
 
-def forward_snowballing():
-    api_url = 'https://api.semanticscholar.org/graph/v1/paper/204e3073870fae3d05bcbc2f6a8e263d9b72e776/citations?fields=title,citationCount,externalIds&offset=0&limit=500'
-    api_text = requests.get(api_url).json() #получили dict из API'шника(JSON-строка)
-    #data = json.loads(api_text)
-    # Извлеките нужные данные
-    for item in api_text["data"]:
-        external_ids = item["citingPaper"]["externalIds"]
-        paper_id = external_ids.get("ArXiv", "N/A")  # Если "ArXiv" отсутствует, возвращает "N/A"
-        citation_count = item["citingPaper"]["citationCount"]
-        print(f"Paper ID: {paper_id}, Citation Count: {citation_count}")
+def forward_snowballing_first_wave(counter_forward:int, api_url:str, api_text: dict, offset: int, limit:int) -> None:
+    if offset + limit < 5000:
+        #print(offset + limit)
+        if (counter_forward == 0):
+            api_url = 'https://api.semanticscholar.org/graph/v1/paper/204e3073870fae3d05bcbc2f6a8e263d9b72e776/citations?fields=title,citationCount,externalIds&offset=0&limit=500'
+            api_text = requests.get(api_url).json() #получили dict из API'шника(JSON-строка)
+        # Извлеките нужные данные
+        for item in api_text["data"]:
+            counter_forward += 1
+            citing_paper = item.get("citingPaper")
+            if citing_paper:
+                external_ids = citing_paper.get("externalIds")
+                if external_ids:
+                    arthive_id = external_ids.get("ArXiv", "N/A")
+                    paper_id = citing_paper.get("paperId", "N/A")
+                    citation_count = citing_paper.get("citationCount", 0)
+                    title = citing_paper.get("title", "N/A")
 
-forward_snowballing()
+            if citation_count > 10  and arthive_id != "N/A":
+                articles_arXiv_ID.add(arthive_id)
+                articles_titles.add(title)
+                articles_paper_ID.add(paper_id)
+                print(f"arXiv ID: {arthive_id}, Citation Count: {citation_count}")
+                print(counter_forward)
+                print(title)
+            if (counter_forward % 500 == 0 and counter_forward != 0):
+                if offset + limit < 9500:
+                    if offset + 499 + limit < 10000:
+                        offset += 499
+                    else:
+                        offset += 500
+                    api_url = "https://api.semanticscholar.org/graph/v1/paper/204e3073870fae3d05bcbc2f6a8e263d9b72e776/citations?fields=title,citationCount,externalIds&offset=" + str(offset) + "&limit=500"
+                    api_text = requests.get(api_url).json()
+                    forward_snowballing_first_wave(counter_forward, api_url, api_text, offset, limit)
+                else:
+                    return
+
+    else:
+        return
+
+
+forward_snowballing_first_wave(0, "", None, 0, limit)
+
+list_articles_titles = list(articles_titles)
+list_articles_arXiv_ID= list(articles_arXiv_ID)
+
+print(list_articles_titles[0])
+for i in range(len(articles_arXiv_ID)):
+    get_mmd("https://arxiv.org//pdf/" + str(list_articles_arXiv_ID[i]) + ".pdf", str(i))
