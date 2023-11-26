e221a68c797cfa5b717cf90637f845eab7ceadf6
Arthur <70059647+ArthurBabkin@users.noreply.github.com>
2023-11-26 14:57:33 +0300

    To merge with 2 wave working

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ limit = 500
 articles_arXiv_ID = set()
 articles_paper_ID = set()
 articles_titles = set()
-articles = []
+
 
 
 def get_pdf(url: str, counter) -> None:
@@ -18,8 +18,7 @@ def get_pdf(url: str, counter) -> None:
     pdf.write(html.content)
 
 def forward_snowballing_first_wave(counter_forward:int, api_url:str, api_text: dict, offset: int, limit:int) -> None:
-    if offset + limit < 5000:
-        #print(offset + limit)
+    if offset + limit < 10000:
         if (counter_forward == 0):
             api_url = 'https://api.semanticscholar.org/graph/v1/paper/204e3073870fae3d05bcbc2f6a8e263d9b72e776/citations?fields=title,citationCount,externalIds&offset=0&limit=500'
             api_text = requests.get(api_url).json() #получили dict из API'шника(JSON-строка)
@@ -35,11 +34,12 @@ def forward_snowballing_first_wave(counter_forward:int, api_url:str, api_text: d
                     citation_count = citing_paper.get("citationCount", 0)
                     title = citing_paper.get("title", "N/A")
 
-            if citation_count > 10  and arthive_id != "N/A":
+            if citation_count > 10 and arthive_id != "N/A":
                 articles_arXiv_ID.add(arthive_id)
                 articles_titles.add(title)
                 articles_paper_ID.add(paper_id)
                 print(f"arXiv ID: {arthive_id}, Citation Count: {citation_count}")
+                print(f"paper ID: {paper_id}")
                 print(counter_forward)
                 print(title)
             if (counter_forward % 500 == 0 and counter_forward != 0):
@@ -53,7 +53,48 @@ def forward_snowballing_first_wave(counter_forward:int, api_url:str, api_text: d
                     forward_snowballing_first_wave(counter_forward, api_url, api_text, offset, limit)
                 else:
                     return
+    else:
+        return
 
+def forward_snowballing_second_wave(counter_forward:int, api_url:str, api_text: dict, offset: int, limit:int) -> None:
+    if offset + limit < 10000:
+        # Извлеките нужные данные
+        for item in api_text["data"]:
+            counter_forward += 1
+            citing_paper = item.get("citingPaper")
+            if citing_paper:
+                external_ids = citing_paper.get("externalIds")
+                if external_ids:
+                    arthive_id = external_ids.get("ArXiv", "N/A")
+                    paper_id = citing_paper.get("paperId", "N/A")
+                    citation_count = citing_paper.get("citationCount", 0)
+                    title = citing_paper.get("title", "N/A")
+
+            if citation_count > 10 and arthive_id != "N/A":
+                articles_arXiv_ID.add(arthive_id)
+                articles_titles.add(title)
+                articles_paper_ID.add(paper_id)
+                print(f"arXiv ID: {arthive_id}, Citation Count: {citation_count}")
+                print(f"paper ID: {paper_id}")
+                print(counter_forward)
+                print(title)
+            if (counter_forward % 500 == 0 and counter_forward != 0):
+                if offset + limit < 9500:
+                    if offset + 499 + limit < 10000:
+                        offset += 499
+                    else:
+                        offset += 500
+                    # Получаем часть строки до параметра offset
+                    url_before_offset = api_url[:api_url.find("&offset=") + 8]
+                    # Получаем часть строки после параметра offset
+                    url_after_offset = api_url[api_url.find("&offset=") + 9:]
+                    api_url = url_before_offset + str(offset) + url_after_offset
+                    api_text = requests.get(api_url).json()
+                    forward_snowballing_first_wave(counter_forward, api_url, api_text, offset, limit)
+                else:
+                    return
+    else:
+        return
 
 
 # Title, ArXiv, citationCount
@@ -76,14 +117,26 @@ def backwards_snowballing():
 
 
 
-backwards_snowballing()
+# backwards_snowballing()
 
 
 forward_snowballing_first_wave(0, "", None, 0, limit)
 
 list_articles_titles = list(articles_titles)
 list_articles_arXiv_ID = list(articles_arXiv_ID)
+list_articles_paper_ID = list(articles_paper_ID)
 
-print(list_articles_titles[0])
-for i in range(len(articles_arXiv_ID)):
-    get_pdf("https://arxiv.org//pdf/" + str(list_articles_arXiv_ID[i]) + ".pdf", str(i))
+print(len(articles_arXiv_ID))
+
+#Осуществляем вторую волну forward snowballing, проходимся по всем элементам articles_paper_ID составленным первой волной
+for i in range(len(list_articles_paper_ID)):
+    api_url = "https://api.semanticscholar.org/graph/v1/paper/" + str(list_articles_paper_ID[i]) + "/citations?fields=title,citationCount,externalIds&offset=0&limit=500"
+    api_text = requests.get(api_url).json()  # получили dict из API'шника(JSON-строка)
+    forward_snowballing_second_wave(0, api_url, api_text, 0, limit)
+
+
+
+print(len(articles_arXiv_ID))
+# for i in range(len(articles_arXiv_ID)):
+#     get_pdf("https://arxiv.org//pdf/" + str(list_articles_arXiv_ID[i]) + ".pdf", str(i))
+#     print(i, " comlete")
